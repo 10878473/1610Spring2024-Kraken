cffa40362e5726481e9a01bfe986bec9cc10f663
Joseph I <10878473@uvu.edu>
2024-01-24 17:07:43 -0700

    Added first files, Not working in VS code yet

---
+++ Operators.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace Operators
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+    	double x = 5;
+        double y = 2;
+        double z = x/y;
+        Console.WriteLine(z);
+      Console.WriteLine("5/2 isnt a whole number!");    
+    }
+  }
+}
---
+++ Variables.cs
@@ -0,0 +1,16 @@
+using System;
+					
+public class Program
+{
+	public static void Main()
+	{
+		double x = 3;
+        double y = 6;
+        double z = x*y;
+        string welcome = "Welcome to the Multiplication station - where we multiply 3 by 6!";
+
+        Console.WriteLine(welcome);
+        Console.WriteLine("3 Times 6 is = " + z);  
+	
+	}
+}
