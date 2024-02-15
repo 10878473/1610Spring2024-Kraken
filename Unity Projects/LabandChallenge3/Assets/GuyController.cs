6c552e3e792f9638f5eb0f432f917ef3a5ae72a1
Joseph Laptop <10878473@uvu.edu>
2024-02-14 17:41:01 -0700

    Setup for lab3

--- Unity Projects/LabandChallenge3/Assets/GuyController.cs
+++ Unity Projects/LabandChallenge3/Assets/GuyController.cs
@@ -2,12 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameController : MonoBehaviour
+public class GuyController : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
+        double moveSpeed = 1;
+        double jumpForce = 1;
+        float gravity = 1f;
+
         
+        Debug.Log("Hello world!");
     }
 
     // Update is called once per frame
