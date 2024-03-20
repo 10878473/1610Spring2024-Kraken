bbed4b4428ba8c148534bb9074c1cbc042cd1caf
Joseph Laptop <10878473@uvu.edu>
2024-03-20 17:17:21 -0600

    Challenge 4 complete
    Easier than expected

--- Unity Projects/Crystal Crusader V4/Assets/Scripts/GameController.cs
+++ Unity Projects/Crystal Crusader V4/Assets/Scripts/GameController.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class GameController : MonoBehaviour
 {
+
+    //This will be UI controller maybe
+    //using UnityEngine.UI;
     // Start is called before the first frame update
     void Start()
     {
