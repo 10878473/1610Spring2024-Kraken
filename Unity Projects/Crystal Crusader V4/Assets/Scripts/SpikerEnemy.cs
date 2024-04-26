641b74e6ac11cd0a33a00c43763f617d5bf388c8
Joseph Laptop <10878473@uvu.edu>
2024-04-26 01:13:52 -0600

    Crystal Crusader- Personal project moving to PC

---
+++ Unity Projects/Crystal Crusader V4/Assets/Scripts/SpikerEnemy.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class SpikerEnemy : MonoBehaviour
+{
+    //
+    //This script will be split into multiple components once i figure out what kind of scripts can be shared
+    //
+    private int hp = 5;
+    private int speed;
+    private GameObject player;
+    private int targetDist;
+    private bool canFire;
+    public GameObject cone;
+    // Start is called before the first frame update
+    void Start()
+    {
+        targetDist = Random.Range(20,50 );
+        player = GameObject.Find("PlayerController");
+        speed = Random.Range(15,30);
+        Debug.Log(gameObject.name + " speed is " + speed);
+        canFire = true;
+    } 
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (hp <= 0)
+        {
+            Destroy(gameObject);
+            //TODO - Explosion?
+        }
+        //rotate towards player? sometimes
+        if(transform.position.z > targetDist)
+        {
+            transform.Translate(Vector3.back* speed * Time.deltaTime, Space.World);//Move towards player. Randomized based on instance?
+        } else
+        {
+            transform.Rotate(player.transform.position - transform.position);
+            if (canFire)
+            {
+                canFire = false;
+                firecones();
+                StartCoroutine("cooldown");
+            }
+        }
+        
+    }
+    private void OnTriggerEnter(Collider other){
+        
+        Debug.Log(gameObject.name +" Collided with _" + other.gameObject.name);
+        
+        if (other.gameObject.CompareTag("Bullet")){
+            hp --;
+            Debug.Log(gameObject.name + "HP:"+hp);
+            Destroy(other.gameObject);
+            //TODO - add visual indicator of damage being taken
+
+        }
+
+    }
+    private void firecones(){
+        Instantiate(cone,transform.position, transform.rotation * Random.rotation);
+    }
+    
+    private IEnumerator cooldown(){
+        yield return new WaitForSeconds(Random.Range(.5f,2));
+        canFire = true;
+    }
+}
---
+++ Unity Projects/Crystal Crusader V4/Assets/TODO.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TODO : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+
+
+
+    // TODO - Swap out all models
+    //Code Spiker behavior - approach, then slow down and fire rounds of spikes in a burst?
+}
